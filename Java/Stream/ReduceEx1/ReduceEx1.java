2293eec08e678edeab4c3a74bdf666a1ddc12863
viniciuslimafernandes <ubuntu@ip-172-31-39-129.us-east-2.compute.internal>
2019-10-16 15:37:17 +0000

    Adicionando ForEach do Stream

--- Java/Stream/ReduceEx1/ReduceEx1.java
+++ Java/Stream/ReduceEx1/ReduceEx1.java
@@ -5,10 +5,16 @@ class ReduceEx1{
 
     public static void main(String[] args){
         List<Integer> myList = Arrays.asList(5,5,5,5);
+        int numArr[] = {5,2,3,4};
         
         int result = myList.stream().reduce(0,(n1,n2) -> n1+n2);
-        int result2 = myList.stream().map(n -> n+5).reduce(0,(n1,n2) -> n1+n2);
+        Integer result2 = myList.stream().map(n -> n+5).reduce(0,(n1,n2) -> n1+n2);
         
-        System.out.println("Resultado: " + result2);
+        int aux = 0;
+        for(int n: numArr){
+            aux += n;
+        }
+        
+        System.out.println("Resultado: " + aux);
     }
 }
